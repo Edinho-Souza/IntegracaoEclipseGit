584afe876d4c4ea252db2bfbcb247b2e0055ac50
Edcarlos Souza <164053620+Edinho-Souza@users.noreply.github.com>
2025-08-01 11:27:22 -0300

    Update IntegrationGit.java
    Teste Pull

--- IntegracaoGit/src/integracaoGit/IntegrationGit.java
+++ IntegracaoGit/src/integracaoGit/IntegrationGit.java
@@ -5,6 +5,7 @@ public class IntegrationGit {
 	public static void main(String[] args) {
 		System.out.println("Integração GIT");
 		System.out.println("Teste1");
+		System.out.println("Teste Pull");
 	}
 
 }
